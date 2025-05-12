f3e48b93d4ec205ed09a7a842865194ce14aa1c4
Kuba Kaliszczak <kuba.kaliszczak@sembot.io>
2025-05-12 13:45:34 +0200

    Cloudflare deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,17 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import vue from '@astrojs/vue';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
-  output: 'server', // Enable server-side rendering for all pages by default
+  // Enable server-side rendering for all pages by default
+  output: 'server',
+
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [vue()]
+
+  integrations: [vue()],
+  adapter: cloudflare()
 });
