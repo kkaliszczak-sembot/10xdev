c1c4d81d600f61d3d91713aeb99d1e571bbb4d50
Kuba Kaliszczak <kuba.kaliszczak@sembot.io>
2025-05-15 22:21:55 +0200

    Remove lucide-vue-next icons

--- src/components/ui/pagination/PaginationNext.vue
+++ src/components/ui/pagination/PaginationNext.vue
@@ -4,7 +4,6 @@ import type { HTMLAttributes } from 'vue'
 import { cn } from '@/lib/utils'
 import { buttonVariants, type ButtonVariants } from '@/components/ui/button'
 import { reactiveOmit } from '@vueuse/core'
-import { ChevronRightIcon } from 'lucide-vue-next'
 import { PaginationNext, useForwardProps } from 'reka-ui'
 
 const props = withDefaults(defineProps<PaginationNextProps & {
@@ -19,14 +18,10 @@ const forwarded = useForwardProps(delegatedProps)
 </script>
 
 <template>
-  <PaginationNext
-    data-slot="pagination-next"
-    :class="cn(buttonVariants({ variant: 'ghost', size }), 'gap-1 px-2.5 sm:pr-2.5', props.class)"
-    v-bind="forwarded"
-  >
+  <PaginationNext data-slot="pagination-next"
+    :class="cn(buttonVariants({ variant: 'ghost', size }), 'gap-1 px-2.5 sm:pr-2.5', props.class)" v-bind="forwarded">
     <slot>
       <span class="hidden sm:block">Next</span>
-      <ChevronRightIcon />
     </slot>
   </PaginationNext>
 </template>
--- src/components/ui/pagination/PaginationPrevious.vue
+++ src/components/ui/pagination/PaginationPrevious.vue
@@ -4,7 +4,6 @@ import type { HTMLAttributes } from 'vue'
 import { cn } from '@/lib/utils'
 import { buttonVariants, type ButtonVariants } from '@/components/ui/button'
 import { reactiveOmit } from '@vueuse/core'
-import { ChevronLeftIcon } from 'lucide-vue-next'
 import { PaginationPrev, useForwardProps } from 'reka-ui'
 
 const props = withDefaults(defineProps<PaginationPrevProps & {
@@ -19,13 +18,9 @@ const forwarded = useForwardProps(delegatedProps)
 </script>
 
 <template>
-  <PaginationPrev
-    data-slot="pagination-previous"
-    :class="cn(buttonVariants({ variant: 'ghost', size }), 'gap-1 px-2.5 sm:pr-2.5', props.class)"
-    v-bind="forwarded"
-  >
+  <PaginationPrev data-slot="pagination-previous"
+    :class="cn(buttonVariants({ variant: 'ghost', size }), 'gap-1 px-2.5 sm:pr-2.5', props.class)" v-bind="forwarded">
     <slot>
-      <ChevronLeftIcon />
       <span class="hidden sm:block">Previous</span>
     </slot>
   </PaginationPrev>
